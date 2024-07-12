fe29f2936c0c6874740368b27d8e4ef4fe9304b3
sohail9972 <2018ece_sohailmaidargi@nie.ac.in>
2024-07-12 14:40:17 +0530

    Script for the Task 2 on WebSites

---
+++ SeleniumProjectTests/src/test/java/TestCases/FindingAutoRecomendKeys.java
@@ -0,0 +1,27 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class FindingAutoRecomendKeys {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Sohail\\Seleniumfolder\\chromedrivernew\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        //amzon,
+
+        driver.get("https://www.amazon.in/");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Key");
+
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='left-pane-results-container']//div[@class='s-suggestion-container']"));
+
+        for (WebElement ele : elements){
+            System.out.println(ele.getText());
+        }
+    }
+
+}
